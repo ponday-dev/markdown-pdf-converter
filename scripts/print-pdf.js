69a3363c07122adcf529709656aea010645b2c23
ponday-dev <ponday.dev+github@gmail.com>
2018-10-16 14:01:15 +0900

    update: B4, B5印刷に対応

--- scripts/print-pdf.js
+++ scripts/print-pdf.js
@@ -6,6 +6,20 @@ module.exports = async function printToPDF(data, options) {
 
     await page.goto('data:text/html;charset=UTF-8,' + data,  { waitUntil: 'networkidle0' });
     await page.emulateMedia('print');
-    await page.pdf(options);
+
+    const formatted = setPageSize(options);
+
+    await page.pdf(formatted);
     await browser.close();
+}
+
+function setPageSize(options) {
+    switch(options.format) {
+        case 'B4':
+            return Object.assign(options, { format: '', width: '257mm', height: '364mm' });
+        case 'B5':
+            return Object.assign(options, { format: '', width: '182mm', height: '257mm' });
+        default:
+            return options;
+    }
 }
