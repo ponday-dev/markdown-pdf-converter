8d827e3d7dab0e48b3d5dc68606cd073bfc270c5
ponday-dev <ponday.dev+github@gmail.com>
2019-03-28 00:53:49 +0900

    update: support vivliostyle

--- js/book/builder.js
+++ js/book/builder.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const mustache = require('mustache');
+const { JSDOM } = require('jsdom');
 
 const markdownParser = require('../markdown/parser');
 const loadFiles = require('../util/load-files');
@@ -17,6 +18,17 @@ function renderTemplate(template, data) {
     return mustache.render(template, data);
 }
 
+function adjustBreakPage({ document }) {
+    for (const elem of document.querySelectorAll('h1 + h2')) {
+        elem.classList.add('first-header');
+    }
+}
+
+function addPageStyle(html, book) {
+    const style = `<style>${fs.readFileSync(book.page_style)}</style>`;
+    return html.replace(/<\/head>/, `${style}</head>`)
+}
+
 module.exports = ({ book }) => {
     const parser = markdownParser(book);
 
@@ -28,5 +40,10 @@ module.exports = ({ book }) => {
     const templatePath = path.resolve(book.templates.path, book.templates.body);
     const template = fs.readFileSync(templatePath, { encoding: 'utf-8' });
 
-    return renderTemplate(template, { body, css });
+    const html = renderTemplate(template, { body, css });
+    const { window } = new JSDOM(html);
+
+    adjustBreakPage(window);
+
+    return addPageStyle(window.document.documentElement.outerHTML, book);
 }
